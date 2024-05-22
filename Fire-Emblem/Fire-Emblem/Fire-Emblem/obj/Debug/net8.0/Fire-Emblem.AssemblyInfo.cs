43eb8d7a352098bc1d0dcca5407b6d1a15fb87e6
Vicente Pareja <vicente.pareja@uc.cl>
2024-05-22 00:48:54 -0400

    Improvement of Attack.cs

--- Fire-Emblem/Fire-Emblem/Fire-Emblem/obj/Debug/net8.0/Fire-Emblem.AssemblyInfo.cs
+++ Fire-Emblem/Fire-Emblem/Fire-Emblem/obj/Debug/net8.0/Fire-Emblem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c2d59b463654e36893e5324839f39b603e9298")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990ba97995fc4d94cf9ae4c9f49b6236d4e33e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
